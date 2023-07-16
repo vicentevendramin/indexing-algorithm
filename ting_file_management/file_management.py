0f05be92984d662ce8cdbe34de7379c6f95ca78f
Vicente Vendramin <guesser.vicente@gmail.com>
2023-07-16 14:20:08 -0300

    feat: requirement 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,12 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith("txt"):
+        return sys.stderr.write("Formato inválido\n")
+    try:
+        with open(path_file, mode="r") as file:
+            file_data = file.read().split("\n")
+            return file_data
+    except FileNotFoundError:
+        return sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
