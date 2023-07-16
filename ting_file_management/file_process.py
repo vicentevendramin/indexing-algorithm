3e93705ae4c5d07ee0a453b7ca7356d69f8fdbe5
Vicente Vendramin <guesser.vicente@gmail.com>
2023-07-16 14:41:56 -0300

    feat: requirement 4

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -19,7 +19,12 @@ def process(path_file, instance):
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    if not instance or instance.__len__() == 0:
+        return sys.stdout.write("Não há elementos\n")
+
+    file_to_delete = instance.dequeue()
+    file_path = file_to_delete["nome_do_arquivo"]
+    sys.stdout.write(f"Arquivo {file_path} removido com sucesso\n")
 
 
 def file_metadata(instance, position):
