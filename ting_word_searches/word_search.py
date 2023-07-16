dff7e1abb61fb2976be18afaeea439d89b079f25
Vicente Vendramin <guesser.vicente@gmail.com>
2023-07-16 15:33:21 -0300

    feat: requirement 8

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -32,7 +32,7 @@ def get_occurrences(content, word, with_content):
     return occurrences
 
 
-def check_word_existence(word, instance, with_content=False):
+def check_word_existence(word, instance, with_content):
     processed_files = []
 
     for index in range(len(instance)):
@@ -48,9 +48,10 @@ def check_word_existence(word, instance, with_content=False):
 
 
 def exists_word(word, instance):
-    processed_files = check_word_existence(word, instance)
+    processed_files = check_word_existence(word, instance, False)
     return processed_files
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    processed_files = check_word_existence(word, instance, True)
+    return processed_files
