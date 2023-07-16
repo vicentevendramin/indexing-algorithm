232523e1780c61a81731913a90b6440ceda88cbc
Vicente Vendramin <guesser.vicente@gmail.com>
2023-07-16 14:12:16 -0300

    feat: requirement 1

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -3,16 +3,20 @@ from ting_file_management.abstract_queue import AbstractQueue
 
 class Queue(AbstractQueue):
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self.list = list()
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self.list)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        return self.list.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        if len(self.list) == 0:
+            return None
+        return self.list.pop(0)
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if index < 0 or index >= len(self.list) or len(self.list) == 0:
+            raise IndexError('Índice Inválido ou Inexistente')
+        return self.list[index]
