efe6fc3429022680d43ec8d3d45c2813bdb927d8
Vicente Vendramin <guesser.vicente@gmail.com>
2023-07-16 14:45:10 -0300

    feat: requirement 5

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -28,4 +28,8 @@ def remove(instance):
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    try:
+        data_info = instance.search(position)
+        sys.stdout.write(str(data_info))
+    except IndexError:
+        sys.stderr.write("Posição inválida")
